7cc257fb4c98f7e787440f5d7579fad2992a867b
Michael Robards <mikerobards@icloud.com>
2018-05-20 14:32:25 -0400

    add makeGrid function

--- designs.js
+++ designs.js
@@ -1,8 +1,24 @@
 // Select color input
+const colorPicker = document.getElementById('colorPicker');
+
 // Select size input
+
+const
 
 // When size is submitted by the user, call makeGrid()
 
+
 function makeGrid() {
-  // Your code goes here!
+  let height = document.getElementById('inputHeight').val();
+  let width = document.getElementById('inputWidth').val();
+  let tableRow = document.getElementsByTag('tr');
+  for (let i = 0; i < height, i++) {
+    table.prepend("<tr></tr>");
+  }
+  for (let j = 0; j < width; j++) {
+    tableRow.prepend("<td></td>");
+  }
+}
+
+
 }
