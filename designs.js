1b90b486eb8c2480159e587e26ad729e7ba4e946
Michael Robards <mikerobards@icloud.com>
2018-05-20 17:24:23 -0400

    pass lab test

--- designs.js
+++ designs.js
@@ -1,40 +1,38 @@
 // Select color input
-const colorPicker = document.getElementById('colorPicker');
+
 const table = document.getElementById('pixelCanvas');
-
-
+const height = document.getElementById('inputHeight');
+const width = document.getElementById('inputWidth');
+const color = document.getElementById('colorPicker');
+const sizePicker = document.getElementById('sizePicker');
 
 // Select size input
-
-const sizePicker = document.getElementById('sizePicker');
 
 sizePicker.addEventListener('submit', function(event) {
   event.preventDefault();
   makeGrid();
-
 });
 
 // adds color to cells when clicked
-table.addEventListener('mousedown', function(event) {
-  const color = colorPicker.value;
-  event.target.style.cssText = `background-color: ${color};`;
-});
 
+table.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName === "TD") {
+    evt.target.style.backgroundColor = color.value;
+  }
+});
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-  if (table.firstChild) {
-    table.innerHTML = '';
-  };
-  const height = document.getElementById('inputHeight').value;
-  const width = document.getElementById('inputWidth').value;
-  for (let i = 0; i < width; i++) {
-    const row = document.createElement('tr');
-    for (let j = 0; j < height; j++) {
-      const cell = document.createElement('td');
-      row.appendChild(cell);
+  table.innerHTML = '';
+  let tr, td;
+  for (let m = 0; m < height.value; m++) {
+    tr = document.createElement('tr');
+    table.appendChild(tr);
+    for (let n = 0; n < width.value; n++) {
+      td = document.createElement('td');
+      tr.appendChild(td);
     }
-    table.appendChild(row);
   }
 }
